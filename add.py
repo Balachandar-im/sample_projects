c92f0ed8cb0bed7588bde17d5b7f3c1e7702da78
Balachandar-im <mbalachandar03@gmail.com>
2025-08-12 21:12:23 +0530

    added few classes

--- add.py
+++ add.py
@@ -8,3 +8,12 @@ class Add:
 
 a = Add(2,3)
 print(a.add())
+
+
+class Multiadd(Add):
+    def __init__(self,a,b,args):
+        super().__init__(a,b)
+        self.args = args
+
+    def mult_add(self):
+        return sum([self.a+self.b],list(self.args))
